06d671b8b2a04108ff9da1abfb81ec832284c6ec
RawnakAra <rawnak96@gmail.com>
2021-10-11 09:35:34 +0300

    cards

--- js/index.js
+++ js/index.js
@@ -1,23 +1,27 @@
-const cards = document.querySelectorAll('.card')
+const cards = document.querySelectorAll('.card'),
+      counter = document.querySelector('.moves'),
+      stars = document.querySelector('.stars')
 
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let moves= 0 ,countCorrectCards=0
 
 function flipCard() {
     if(lockBoard)return
     if(this === firstCard)return;
     this.classList.toggle('flip');
-
+ 
     if (!hasFlippedCard) {
         hasFlippedCard = true
         firstCard = this
-        // console.log(hasFlippedCard,firstCard);
+        moveCounter()
         return;
     }
         hasFlippedCard = false;
         secondCard = this;
         checkForMatch()
+        moveCounter()
     
 }
 
@@ -27,6 +31,11 @@ function checkForMatch() {
     isMatch ? disableCards() : unflipCards();
 }
 
+// function conuntMatch(){
+//     if(checkForMatch())
+//     countCorrectCards ++;
+// }
+//console.log(conuntMatch())
 function disableCards(){
     firstCard.removeEventListener('click', firstCard)
     secondCard.removeEventListener('click', firstCard)
@@ -46,7 +55,29 @@ function resetBoard(){
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
 }
+function moveCounter(){
+    moves++;
+    counter.innerHTML = moves;
+    //console.log(moves)
+    if (moves > 8 && moves < 12){
+        for( i= 0; i < 3; i++){
+            if(i > 1){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+    else if (moves > 13){
+        for( i= 0; i < 3; i++){
+            if(i > 0){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+}
 
+// function congratulations(){
+//     if()
+// }
 (function shuffle(){
     cards.forEach(card =>{
         let randomPos = Math.floor(Math.random()*12);
